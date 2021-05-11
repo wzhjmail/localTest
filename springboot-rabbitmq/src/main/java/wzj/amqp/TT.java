7ba5ba228b8ddc551b459ee7482bde582dbb5200
wangzhengjun <wangzhengjun@meicai.cn>
2021-05-11 15:44:18 +0800

    引入sentinel实例

---
+++ springboot-rabbitmq/src/main/java/wzj/amqp/TT.java
@@ -0,0 +1,15 @@
+package wzj.amqp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class TT {
+    @Getter
+    @Setter
+    Integer age;
+
+    public static void main(String args[]) {
+        String st = "w3";
+        Integer.parseInt(st);
+    }
+}
